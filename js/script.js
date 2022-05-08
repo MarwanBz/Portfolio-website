954b029396c3de8717c3aa0b3f5b27fa6847b712
Marwan <kaka.mer1998@gmail.com>
2022-05-08 14:48:55 +0300

    minor changes

--- js/script.js
+++ js/script.js
@@ -1,22 +1,39 @@
-function navSlide() {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector("header .container .nav ul.nav-links");
-  const navLinks = document.querySelectorAll("header .container .nav ul.nav-links li")
-  burger.addEventListener("click",()=> {
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("header .container .nav ul.nav-links");
+const navLinks = document.querySelectorAll("header .container .nav ul.nav-links li");
+const sections = document.querySelectorAll("section");
+console.log(sections);
 
+function navSlide() {
+  burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
 
-    burger.classList.toggle('toggle');
-    // links animation fade 
-    navLinks.forEach((link ,index) => {
-      if(link.style.animation) {
-        link.style.animation = '';
-      }
-      else 
-      {
-        link.style.animation = `navfade 0.5s ease forwards ${index /7 + 0.5}s`;
+    burger.classList.toggle("toggle");
+    // links animation fade
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navfade 0.5s ease forwards ${index / 7 + 0.5}s`;
       }
     });
   });
 }
+
+// window.onscroll = () => {
+//   var current = "";
+//   sections.forEach((section) => {
+//     const sectionTop = section.offsetTop;
+//     if (scrollY >= sectionTop - 20) {
+//       current = section.getAttribute("id");
+//     }
+//   });
+//   navLinks.forEach((li) => {
+//     li.classList.remove("active");
+//     if (li.classList.contains(current)) {
+//       // console.log(current);
+//       li.classList.add("active");
+//     }
+//   });
+// };
 navSlide();
